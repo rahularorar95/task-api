f769e8c81ad79e6829efd8f951e2ff407a4c25d3
Rahul <rahularorar95@gmail.com>
2021-01-20 17:32:57 +0530

    Added headerbar in dashboard

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Header from '../Header';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    }
+  }));
 
 function Dashboard() {
+
+    const classes = useStyles();
     return (
-        <div>
-            Dashboard
+        <div className={classes.root}>
+            
+            {/* Header */}
+            <Header/>
+            
+            {/* DashboardStats */}
+
+            {/* TasksList */}
+
         </div>
     )
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import { Button } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    height: 64,
+  },
+  username: {
+    border: "1px solid black",
+    flexGrow: 1,
+    textAlign: "start",
+    color: "#537278",
+  },
+  profileImg: {
+    borderRadius: 24,
+  },
+  layoutMargin: {
+    marginLeft: theme.spacing(8),
+    marginRight: theme.spacing(8),
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+      marginRight: 0,
+    },
+  },
+}));
+
+function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar color="inherit" position="static">
+        <Toolbar className={classes.layoutMargin}>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <img
+              height={48}
+              className={classes.profileImg}
+              src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+              alt="profile"
+            />
+          </IconButton>
+          <Typography variant="h6" className={classes.username}>
+            Rahul
+          </Typography>
+          <Button>
+            <span style={{ color: "#537278" }}>Logout</span>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;
