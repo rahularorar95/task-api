f2a3e70d83e5443d167adee20a0eb14b2669eb91
Rahul <rahularorar95@gmail.com>
2021-01-22 00:21:21 +0530

    fixed exhaustive-deps warning

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -32,6 +32,7 @@ function Dashboard({ username }) {
           redirectToLogin();
         }
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const redirectToLogin = () => {
